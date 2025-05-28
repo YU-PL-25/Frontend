d627f35a56841c75c4c3e70be8d0b2f78f23c699
Woohyeon Roh <ykjrco@naver.com>
2025-05-28 13:41:55 +0900

    [추가] 메인 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/App.css';
 import Landing from './pages/Landing';
+import Main from './pages/Main';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/landing" replace/>}/>
         <Route path="/landing" element={<Landing/>}/>
+        <Route path="/main" element={<Main/>}/>
       </Routes>
     </Router>
   );
