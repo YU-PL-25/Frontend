703f7d0ee0cb346cb6411b6211d28ab767de0b39
Woohyeon Roh <ykjrco@naver.com>
2025-05-28 14:04:41 +0900

    [추가] 메인페이지 Header 컴포넌트 및 전용 CSS 스타일링 추가

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import logoImage from '../assets/shuttleplay_main_logo.png';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <Link to="/main">
+          <img src={logoImage} alt="ShuttlePlay Logo" className="logo-image"/>
+        </Link>
+      </div>
+
+      <nav className="header-center">
+        <ul className="nav-menu">
+          <li><Link to="/pre-matching">사전 매칭</Link></li>
+          <li><Link to="/current-matching">현장 매칭</Link></li>
+          <li><Link to="/competition">대회 관리</Link></li>
+          <li><Link to="/reservation">구장 예약</Link></li>
+          <li><Link to="/club">모임 관리</Link></li>
+          <li><Link to="/community">커뮤니티</Link></li>
+        </ul>
+      </nav>
+
+      <div className="header-right">
+        <i className="bi bi-chat-dots" title="채팅"></i>
+        <i className="bi bi-bell" title="알림"></i>
+        <button className="login-btn">
+          <Link to="/login" className="login-link">로그인</Link>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
