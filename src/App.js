9f99382e2a23c39978a8d9a1fdbd07d4cec6449a
Woohyeon Roh <ykjrco@naver.com>
2025-05-28 18:31:10 +0900

    [추가] App.js에 임시 페이지 라우팅 경로 설정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import './styles/App.css';
 import Landing from './pages/Landing';
 import Main from './pages/Main';
+import Register from './pages/Register';
+import PreMatching from './pages/PreMatching';
+import CurrentMatching from './pages/CurrentMatching';
+import Competition from './pages/Competition';
+import Reservation from './pages/Reservation';
+import Club from './pages/Club';
+import Community from './pages/Community';
 
 function App() {
   return (
@@ -11,6 +18,13 @@ function App() {
         <Route path="/" element={<Navigate to="/landing" replace/>}/>
         <Route path="/landing" element={<Landing/>}/>
         <Route path="/main" element={<Main/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/pre-matching" element={<PreMatching/>}/>
+        <Route path="/current-matching" element={<CurrentMatching/>}/>
+        <Route path="/competition" element={<Competition/>}/>
+        <Route path="/reservation" element={<Reservation/>}/>
+        <Route path="/club" element={<Club/>}/>
+        <Route path="/community" element={<Community/>}/>
       </Routes>
     </Router>
   );
