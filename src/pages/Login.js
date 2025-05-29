efa77690f830dcebe966b07490190f25f94ff012
Woohyeon Roh <ykjrco@naver.com>
2025-05-29 09:40:12 +0900

    [수정] 클래스명 중복 문제 처리

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,29 +4,27 @@ import '../styles/Login.css';
 import logo from '../assets/shuttleplay_main_logo.png';
 
 function LoginPage() {
-
-
   return (
     <div className="login-wrapper">
       <Link to="/main" className="Back-btn">이전</Link>
       <Link to="/main">
-        <img src={logo} alt="로고" className="logo-img" />
+        <img src={logo} alt="로고" className="logo-img"/>
       </Link>
       <h2 className="subtitle">로그인</h2>
 
       <form method="POST" action="/login" className="login-form">
         <label htmlFor="username">아이디</label>
-        <input type="text" id="username" name="username" placeholder="아이디" required />
+        <input type="text" id="username" name="username" placeholder="아이디" required/>
 
         <label htmlFor="password">비밀번호</label>
-        <input type="password" id="password" name="password" placeholder="비밀번호" required />
+        <input type="password" id="password" name="password" placeholder="비밀번호" required/>
 
         <div className="options">
-          <label><input type="checkbox" name="remember" /> 자동로그인</label>
+          <label><input type="checkbox" name="remember"/> 자동로그인</label>
           <a href="#!" className="find-link">정보찾기</a>
         </div>
 
-        <button type="submit" className="login-btn">로그인</button>
+        <button type="submit" className="login-submit-btn">로그인</button>
       </form>
 
       <div className="social-buttons-grid">
