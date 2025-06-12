b5820852bee62a0a9c459f0038a191c975072012
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-06-12 22:56:14 +0900

    [삭제] 사용하지 않는 Link import 제거

--- src/pages/PreMatching.js
+++ src/pages/PreMatching.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/PreMatching.css';
